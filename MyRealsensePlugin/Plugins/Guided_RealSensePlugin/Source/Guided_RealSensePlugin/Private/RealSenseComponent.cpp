d8bc395a6db013db31052b1c520d500a17d8f503
Adrian Vorlaufer <adrian.vorlaufer@sensodrive.de>
2018-08-13 16:36:25 +0200

    started transforming stream to texture

--- MyRealsensePlugin/Plugins/Guided_RealSensePlugin/Source/Guided_RealSensePlugin/Private/RealSenseComponent.cpp
+++ MyRealsensePlugin/Plugins/Guided_RealSensePlugin/Source/Guided_RealSensePlugin/Private/RealSenseComponent.cpp
@@ -12,7 +12,6 @@ URealSenseComponent::URealSenseComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -28,7 +27,56 @@ void URealSenseComponent::BeginPlay()
 	else {
 		UE_LOG(Guided_RealSensePlugin, Warning, TEXT("IGuided_RealSensePlugin not available"));
 	}
-	
+
+
+	try {
+
+		pipeline = new rs2::pipeline();
+		pipeline->start();
+		cameraWorks = true;
+	}
+	catch (std::exception e) {
+		UE_LOG(Guided_RealSensePlugin, Error, TEXT("Realsense initialization error: %s"), e.what());
+	}
+
+	try {
+		rs2::frameset frames = pipeline->wait_for_frames();
+		rs2::video_frame colorFrame = frames.get_color_frame();
+		TextureVideo = UTexture2D::CreateTransient(colorFrame.get_width(), colorFrame.get_height());
+		TextureVideo->AddToRoot();
+		TextureVideo->UpdateResource();
+		textureVideoRegion = new FUpdateTextureRegion2D(0,0,0,0, colorFrame.get_width(), colorFrame.get_height());
+
+	}
+	catch (std::exception e) {
+		UE_LOG(Guided_RealSensePlugin, Error, TEXT("Realsense video stream error: %s"), e.what());
+		cameraWorks = false;
+	}
+}
+
+bool URealSenseComponent::receiveFrame()
+{
+	try {
+		rs2::frameset frames = pipeline->wait_for_frames();
+		rs2::video_frame colorFrame = frames.get_color_frame();
+		uint8* RawUndistortedLeftBGRA = (uint8*)colorFrame.get_data();
+		int UndistortedFrameIndex = colorFrame.get_frame_number();
+		int UndistortedTimeIndex = colorFrame.get_frame_timestamp_domain();
+
+		TextureVideo->updateTextureRegion(DBL_MAX_10_EXP, 1, textureVideoRegion, Cast<uint32>(colorFrame.get_width * sizeof(uint8) * 4), sizeof(uint8) * 4, RawUndistortedLeftBGRA, texCleanUpFP);
+	}
+	catch (const rs2::error & e) {
+
+		UE_LOG(Guided_RealSensePlugin, Error, TEXT("%s"), *FString(e.what()));
+		cameraWorks = false;
+	}
+	catch (const std::exception& e) {
+
+		UE_LOG(Guided_RealSensePlugin, Error, TEXT("%s"), *FString(e.what()));
+		cameraWorks = false;
+	}
+
+	return cameraWorks;
 }
 
 
@@ -37,6 +85,8 @@ void URealSenseComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (cameraWorks) {
+		receiveFrame();
+	}
 }
 
--- MyRealsensePlugin/Plugins/Guided_RealSensePlugin/Source/Guided_RealSensePlugin/Public/RealSenseComponent.h
+++ MyRealsensePlugin/Plugins/Guided_RealSensePlugin/Source/Guided_RealSensePlugin/Public/RealSenseComponent.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Components/Image.h"
 #include "RealSenseComponent.generated.h"
+
+#include <rs.hpp>
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -19,14 +22,27 @@ public:
 	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Guided_RealSensePlugin")
 	FString filePath;
 
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Guided_RealSensePlugin")
+		UTexture2D* TextureVideo = nullptr;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	bool receiveFrame();
+
+	FUpdateTextureRegion2D* textureVideoRegion = nullptr;
+
+	rs2::pipeline* pipeline = nullptr;
+
+	void TextureRegionCleanUp(uint8* rawData, const FUpdateTextureRegion2D* region) {};
+	TFunction<void(uint8*, const FUpdateTextureRegion2D*)> texCleanUpFP = [this](uint8* rawData, const FUpdateTextureRegion2D* region) { TextureRegionCleanUp(rawData, region); };
+
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	bool cameraWorks = false;
 	
 };
